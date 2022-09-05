39e9977bae8055c24cc1c84d43a4565808d7f05d
thomassamoth <thomassamoth.dev@gmail.com>
2022-09-05 15:31:03 +0200

    refactor: apply black style

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup
 
 with open("README.md", "r") as f:
-    long_description= f.read()
-    
-setup(name="ansiconverter",
-      version="0.0.1.1",
-      description="Convert any color in RGB or hexadecimal format to ANSI code",
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      author="Thomas Beyet",
-      author_email="",
-      url="https://github.com/thomassamoth/ansiconverter",
-      package_dir={"": "src"},
-      classifiers=[
+    long_description = f.read()
+
+setup(
+    name="ansiconverter",
+    version="0.0.1",
+    description="Convert any color in RGB or hexadecimal format to ANSI code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Thomas Beyet",
+    author_email="",
+    url="https://github.com/thomassamoth/ansiconverter",
+    package_dir={"": "src"},
+    classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
@@ -21,8 +22,9 @@ setup(name="ansiconverter",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-      extras_require= {
-        "dev":["pytest>=3.6",],}
-      )
-
-print(long_description)
+    extras_require={
+        "dev": [
+            "pytest>=3.6",
+        ],
+    },
+)
