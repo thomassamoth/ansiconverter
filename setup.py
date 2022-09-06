47f3c8a5a9b544e682def2f56127fa3fdea2f329
thomassamoth <thomassamoth.dev@gmail.com>
2022-09-06 16:16:32 +0200

    fix: add the necessary py file the create the whl + some more informations

--- setup.py
+++ setup.py
@@ -5,20 +5,23 @@ with open("README.md", "r") as f:
 
 setup(
     name="ansiconverter",
-    version="0.0.1",
+    version="0.0.2",
     description="Convert any color in RGB or hexadecimal format to ANSI code",
+    license='MIT',
     long_description=long_description,
     long_description_content_type="text/markdown",
+    py_modules=["ansiconverter"],
     author="Thomas Beyet",
-    author_email="",
+    author_email=None,
     url="https://github.com/thomassamoth/ansiconverter",
     package_dir={"": "src"},
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        'Development Status :: 3 - Alpha',
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
@@ -27,4 +30,5 @@ setup(
             "pytest>=3.6",
         ],
     },
+    project_urls={'source': 'http://github.com/thomassamoth/ansiconverter', }
 )
