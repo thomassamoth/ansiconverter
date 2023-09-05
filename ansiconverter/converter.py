2d77787e952ddd496bbd79a86eeaeff362718f2d
thomassamoth <thomassamoth.dev@gmail.com>
2023-09-05 22:07:03 +0200

    feat: simplify styles & rework of the structure
    Regroup functions by classes and simplify the import for the styles

--- ansiconverter/converter.py
+++ ansiconverter/converter.py
@@ -4,88 +4,82 @@ Also notice that some colors can't be mixed together as foreground and backgroun
 """
 
 
-RESET = "\x1b[0m"
+class Converter:
+    """ANSI conversions and an additional function to convert RGB to HEX."""
 
+    RESET = "\x1b[0m"
 
-# class converter:
-"""ANSI conversions and an additional function to convert RGB to HEX."""
+    @staticmethod
+    def RGBtoANSI(
+        text: str,
+        foregound=[255, 255, 255],
+        background=[],
+    ):
+        """Write a text in RGB color.
 
-RESET = "\x1b[0m"
+        Args:
+            text (str): the text you want to write.
+            foregound (list, optional): RGB foregound's color. Defaults to [255, 255, 255].
+            background (list, optional): RGB background's color. Defaults to [].
 
+        Raises:
+            ValueError: the foregound color can't be an empty list.
 
-def RGBtoANSI(
-    text: str,
-    foregound=[255, 255, 255],
-    background=[],
-):
-    """Write a text in RGB color.
-
-    Args:
-        text (str): the text you want to write.
-        foregound (list, optional): RGB foregound's color. Defaults to [255, 255, 255].
-        background (list, optional): RGB background's color. Defaults to [].
-
-    Raises:
-        ValueError: the foregound color can't be an empty list.
-
-    Returns:
-        string: the ANSI code for the foreground and the background.
-    """
-    if foregound != []:
-        if background == []:
-            return f"\033[38;2;{foregound[0]};{foregound[1]};{foregound[2]}m{str(text)}{RESET}"
+        Returns:
+            string: the ANSI code for the foreground and the background.
+        """
+        if foregound != []:
+            if background == []:
+                return f"\033[38;2;{foregound[0]};{foregound[1]};{foregound[2]}m{str(text)}{Converter.RESET}"
+            else:
+                return f"\033[38;2;{foregound[0]};{foregound[1]};{foregound[2]}m\033[48;2;{background[0]};{background[1]};{background[2]}m{str(text)}{Converter.RESET}"
         else:
-            return f"\033[38;2;{foregound[0]};{foregound[1]};{foregound[2]}m\033[48;2;{background[0]};{background[1]};{background[2]}m{str(text)}{RESET}"
-    else:
-        raise ValueError(
-            "The foregound can't be an empty list!\nNo paramaters will write the text in write"
-        )
+            raise ValueError(
+                "The foregound can't be an empty list!\nNo paramaters will write the text in write"
+            )
 
+    @staticmethod
+    def HEXtoRGB(fg="#000000"):
+        """Convert a hexadecimal color to its RGB triplet.
 
-def HEXtoRGB(fg="#000000"):
-    """Convert a hexadecimal color to its RGB triplet.
+        Args:
+            fg (str, optional): Hexadecimal color value. Defaults to "#000000".
 
-    Args:
-        fg (str, optional): Hexadecimal color value. Defaults to "#000000".
+        Raises:
+            ValueError: The color is not a correct hexadecimal value.
 
-    Raises:
-        ValueError: The color is not a correct hexadecimal value.
+        Returns:
+            list: triplet of RGB values.
+        """
+        foreground = ""
+        while True:
+            foreground = fg.lstrip("#").lower()
+            if len(foreground) == 6:
+                return list(int(foreground[i : i + 2], base=16) for i in (0, 2, 4))
 
-    Returns:
-        list: triplet of RGB values.
-    """
-    foreground = ""
-    while True:
-        foreground = fg.lstrip("#").lower()
-        if len(foreground) == 6:
-            return list(int(foreground[i : i + 2], base=16) for i in (0, 2, 4))
+            else:
+                raise ValueError("Enter a valid hexadecimal value")
+                return 1
+
+    @staticmethod
+    def HEXtoANSI(text, foreground="#ffffff", background=""):
+        from ansiconverter import HEXtoRGB, RGBtoANSI
+
+        foregroundRGB = HEXtoRGB(foreground)
+        if foreground != "":
+            if background != "":
+                backgroundRGB = HEXtoRGB(background)
+                return RGBtoANSI(text, foregroundRGB, backgroundRGB)
+            else:
+                return RGBtoANSI(text, foregroundRGB)
 
         else:
-            raise ValueError("Enter a valid hexadecimal value")
-            return 1
+            raise ValueError("Please enter at least one foreground color.")
 
-
-def HEXtoANSI(text, foreground="#ffffff", background=""):
-    from ansiconverter.converter import HEXtoRGB
-
-    foregroundRGB = HEXtoRGB(foreground)
-    if foreground != "":
-        if background != "":
-            backgroundRGB = HEXtoRGB(background)
-            print(foregroundRGB, backgroundRGB)
-            return RGBtoANSI(text, foregroundRGB, backgroundRGB)
+    @staticmethod
+    # Utility to convert to other color format
+    def RGBtoHEX(rgb=[255, 255, 255]):
+        if rgb != []:
+            return f"#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}"
         else:
-            return RGBtoANSI(text, foregroundRGB)
-
-    else:
-        raise ValueError("Please enter at least one foreground color.")
-
-
-# Utility to convert to other color format
-
-
-def RGBtoHEX(rgb=[255, 255, 255]):
-    if rgb != []:
-        return f"#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}"
-    else:
-        raise (ValueError(f"The color can't be an empty list. Please retry."))
+            raise ValueError(f"The color can't be an empty list. Please retry.")
