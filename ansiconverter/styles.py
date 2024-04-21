ab4f02360eadec1494e4a985221599fc6c148eb4
thomassamoth <thomassamoth.dev@gmail.com>
2024-04-21 19:02:38 +0200

    refactor(styles): use f-strings instead of concatenate

--- ansiconverter/styles.py
+++ ansiconverter/styles.py
@@ -21,25 +21,24 @@ class Styles:
 
     @staticmethod
     def faint(text):
-        return "\x1b[2m" + text + Styles.RESET
+        return f"\x1b[2m{text}{Styles.RESET}"
 
     @staticmethod
     def italic(text):
-        return "\x1b[3m" + text + Styles.RESET
+        return f"\x1b[3m{text}{Styles.RESET}"
 
     @staticmethod
     def underline(text):
-        return "\x1b[4m" + text + Styles.RESET
+        return f"\x1b[4m{text}{Styles.RESET}"
 
     @staticmethod
     def bold_and_underline(text):
-        return "\x1b[1;4m" + text + Styles.RESET
+        return f"\x1b[1;4m{text}{Styles.RESET}"
 
     @staticmethod
     def strikethrough(text):
-        return "\x1b[9m" + text + Styles.RESET
+        return f"\x1b[9m{text}{Styles.RESET}"
 
     @staticmethod
     def reverse(text):
-        return "\x1b[7m" + text + Styles.RESET
-
+        return f"\x1b[7m{text}{Styles.RESET}"
