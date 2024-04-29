5168548d0a52ba380136c66cc1c08c9538c025ac
thomassamoth <thomassamoth.dev@gmail.com>
2024-04-29 21:16:06 +0200

    allow to use list and tuples in the functions
    This ensures that user with previous version still works

--- ansiconverter/converter.py
+++ ansiconverter/converter.py
@@ -11,29 +11,35 @@ class Converter:
 
     #fmt: off
     @staticmethod
-    def RGBtoANSI(text: str,foregound=(255, 255, 255),background=(),):
+    def RGBtoANSI(text: str,foreground=(255, 255, 255),background=(),):
         """Write a text in RGB colour.
 
         Args:
             text (str): the text you want to write.
-            foregound (tuple, optional): RGB foregound's colour. Defaults to (255, 255, 255).
+            foreground (tuple, optional): RGB foreground's colour. Defaults to (255, 255, 255).
             background (tuple, optional): RGB background's colour. Defaults to ().
 
         Raises:
-            ValueError: the foregound colour can't be an empty list.
+            ValueError: the foreground colour can't be an empty list.
 
         Returns:
             string: the ANSI code for the foreground and the background.
         """
-    #fmt: on
-        if foregound != ():
+        #fmt: on
+        if isinstance(foreground, list):
+            foreground = tuple(foreground)
+            
+        if isinstance(background, list):
+            background = tuple(background)
+
+        if foreground != ():
             if background == ():
-                return f"\033[38;2;{foregound[0]};{foregound[1]};{foregound[2]}m{str(text)}{Converter.RESET}"
+                return f"\033[38;2;{foreground[0]};{foreground[1]};{foreground[2]}m{str(text)}{Converter.RESET}"
             else:
-                return f"\033[38;2;{foregound[0]};{foregound[1]};{foregound[2]}m\033[48;2;{background[0]};{background[1]};{background[2]}m{str(text)}{Converter.RESET}"
+                return f"\033[38;2;{foreground[0]};{foreground[1]};{foreground[2]}m\033[48;2;{background[0]};{background[1]};{background[2]}m{str(text)}{Converter.RESET}"
         else:
             raise ValueError(
-                "The foregound can't be an empty list!\nNo paramaters will write the text in write"
+                "The foreground can't be an empty list!\nNo paramaters will write the text in write"
             )
 
     @staticmethod
@@ -56,11 +62,26 @@ class Converter:
                 return tuple(int(foreground[i : i + 2], base=16) for i in (0, 2, 4))
 
             else:
-                raise ValueError("Enter a valid hexadecimal value")
+                raise ValueError("Enter a valid hexadecimal value.")
                 return 1
 
     @staticmethod
     def HEXtoANSI(text, foreground="#ffffff", background=""):
+        """Converts text in hexadecimal colour into an ANSI representation.
+
+        Args:
+            text (str): The text to convert.
+            foreground (str, optional): The hexadecimal colour of the text. Defaults to "#ffffff".
+            background (str, optional): The hexadecimal colour of the background. Defaults to "".
+
+        Raises:
+            ValueError: _description_
+            ValueError: _description_
+
+        Returns:
+            str: the converter to ANSI sequence with the correct colours.
+        """
+
         from ansiconverter import HEXtoRGB, RGBtoANSI
 
         foregroundRGB = HEXtoRGB(foreground)
@@ -77,7 +98,15 @@ class Converter:
     @staticmethod
     # Utility to convert to other colour format
     def RGBtoHEX(rgb=(255, 255, 255)):
+        if isinstance(rgb, list):
+            rgb = tuple(rgb)
+        
         if rgb != ():
+            for color in rgb:
+                if not 0 <= color < 256:
+                    raise ValueError("Each component must be in the range [0, 255].")
+            
+            # Convert to hexadecimal
             return f"#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}"
         else:
             raise ValueError(f"The colour can't be empty. Please retry.")
