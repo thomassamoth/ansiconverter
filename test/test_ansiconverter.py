3bc1a6c834289898937bc605ec82cb06163cd9db
thomassamoth <25958977+thomassamoth@users.noreply.github.com>
2024-05-20 00:05:33 +0200

    refactor: better tests

--- test/test_ansiconverter.py
+++ test/test_ansiconverter.py
@@ -1,35 +1,71 @@
 import pytest
-from ansiconverter import *
+from ansiconverter import RGBtoHEX, RGBtoANSI, HEXtoRGB, HEXtoANSI
 
 
 class TestsConverter:
     """Tests for the converter module."""
 
-    def test_RGBtoHEX_valid_input(self):
+    @classmethod
+    def setup_class(cls):
+        cls.rgb_color = (13, 24, 128)
+        cls.hex_color = "#0d1880"
+        cls.text = "Valid Input"
+
+    @pytest.mark.parametrize(
+        "input_data, expected",
+        [
+            ((13, 24, 128), "#0d1880"),
+            ([13, 24, 128], "#0d1880"),
+        ],
+    )
+    def test_RGBtoHEX_valid_input(self, input_data, expected):
+        """Test the conversion from RGB to HEX with a correct list of colours"""
+        assert RGBtoHEX(input_data) == expected
+
+    @pytest.mark.parametrize(
+        "input_data, expected",
+        [
+            ((0, 10, 24), (255, 255, 255)),
+            ([0, 10, 24], [255, 255, 255]),
+        ],
+    )
+    def test_RGBtoHEX_list_input(self, input_data, expected):
         """Test the conversion from RGB to HEX with a correct list of colours"""
         assert RGBtoHEX((13, 24, 128)) == "#0d1880"
-        
-    def test_RGBtoHEX_list_input(self):
-        """Test the conversion from RGB to HEX with a correct list of colours"""
-        assert RGBtoHEX([13, 24, 128]) == "#0d1880"
 
     def test_RGBtoHEX_out_of_range_value(self):
         with pytest.raises(ValueError) as e:
             RGBtoHEX((355, -2, 256))
-        
-    def test_RGBtoANSI_blank_list(self):
-        """Test the conversion from RGB to ANSI with no argument. It should raise an error"""
-        with pytest.raises(ValueError) as e:
-            RGBtoANSI("text", ())
 
     def test_RGBtoANSI_valid_input(self):
-        """Test RGB to ANSI with a valid text and no foreground color. (If printed text appears white.)
-        """
-        assert RGBtoANSI("Valid Input", (125, 125, 125)) == "\x1B[38;2;125;125;125mValid Input\x1b[0m"
+        """Test RGB to ANSI with a valid text and no foreground color. (If printed text appears white.)"""
+        assert (
+            RGBtoANSI("Valid Input", (125, 125, 125))
+            == "\x1B[38;2;125;125;125mValid Input\x1b[0m"
+        )
 
     def test_HEXtoRGB_valid_input(self):
         """Test from HEX to RGB with a correct string"""
         assert HEXtoRGB("#1b815a") == (27, 129, 90)
+
+    def test_HEXtoRGB_2_sharp(self):
+        """Test from HEX to RGB with 2 # entered (i.e after a copy/paste)"""
+        assert HEXtoRGB("##1b815a") == (27, 129, 90)
+
+    def test_RGBtoANSI_background(self):
+        assert (
+            RGBtoANSI("Test", (0, 10, 24), (255, 255, 255))
+            == f"\033[38;2;{0};{10};{24}m\033[48;2;{255};{255};{255}mTest\x1b[0m"
+        )
+
+    def test_RGBtoANSI_background_list(self):
+        assert (
+            RGBtoANSI("Test", [0, 10, 24], [255, 255, 255])
+            == f"\033[38;2;{0};{10};{24}m\033[48;2;{255};{255};{255}mTest\x1b[0m"
+        )
+
+    def test_HEXtoANSI_valid_input(self):
+        assert HEXtoANSI("Test HEX to ANSI", "#fdd374")
 
     def test_HEXtoRGB_shorter_value(self):
         """Test from HEX to RGB with string shorter than 6 characters"""
@@ -41,20 +77,11 @@ class TestsConverter:
         with pytest.raises(ValueError) as e:
             HEXtoRGB("#1b815ad")
 
-    def test_HEXtoRGB_2_sharp(self):
-        """Test from HEX to RGB with 2 # entered (i.e after a copy/paste)"""
-        assert HEXtoRGB("##1b815a") == (27, 129, 90)
-        
-    def test_RGBtoANSI_background(self):
-        assert RGBtoANSI("Test", (0, 10, 24), (255, 255, 255)) == f"\033[38;2;{0};{10};{24}m\033[48;2;{255};{255};{255}mTest\x1b[0m"
-    
-    def test_RGBtoANSI_background_list(self):
-        assert RGBtoANSI("Test", [0, 10, 24], [255, 255, 255]) == f"\033[38;2;{0};{10};{24}m\033[48;2;{255};{255};{255}mTest\x1b[0m"
+    def test_RGBtoANSI_blank_list(self):
+        """Test the conversion from RGB to ANSI with no argument. It should raise an error"""
+        with pytest.raises(ValueError) as e:
+            RGBtoANSI("text", ())
 
-    def test_HEXtoANSI_valid_input(self):
-        assert HEXtoANSI("Test HEX to ANSI", "#fdd374")
-        
     def test_HEXtoANSI_no_foreground(self):
         with pytest.raises(ValueError) as e:
             HEXtoANSI("Text", "")
-            
